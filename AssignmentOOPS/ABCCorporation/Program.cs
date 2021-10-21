8cc0528ab80d3ff35a698ca4cdff8af97be39f40
saisandhya36 <89247527+saisandhya36@users.noreply.github.com>
2021-10-21 21:39:00 +0530

    Add files via upload

---
+++ AssignmentOOPS/ABCCorporation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ABCCorporation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Employee Object
+            Employee emp = new Employee();
+            Employee.EmpName = "sandhya";
+            
+
+
+
+            Console.WriteLine(Employee);
+
+
+
+            employee.Deposit(5000);
+            Console.WriteLine(employee.ToString());
+
+
+
+            employee.Withdraw(3000);
+            Console.WriteLine(employee);
+
+
+        }
+    }
+}
---
+++ AssignmentOOPS/HRLib/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+
+namespace HRLib
+{
+    public class Employee
+    {
+        #region Fields & Properties
+
+
+
+        private string empName;
+
+
+
+        public string EmpName
+        {
+            get { return empName; }
+            set { empName = value; }
+        }
+
+
+
+        private string address;
+
+
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        #endregion
+
+
+
+
+
+        #region Constructor
+
+
+
+        public Employee()
+        {
+            empName="sandhya";
+            address = "kkd";
+        }
+
+
+
+        public Employee(string ename, string eaddr)
+        {
+            this.EmpName = ename;
+            this.Address = eaddr;
+        }
+        #endregion
+
+
+
+
+
+
+
+    }
+}
