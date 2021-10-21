225e18c12519780b414007c4a9b231a1167299e2
saisandhya36 <89247527+saisandhya36@users.noreply.github.com>
2021-10-21 21:52:35 +0530

    Add files via upload

---
+++ EFAssessment/EFAssessment/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace EFAssessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(145, 138);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(94, 25);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "UserName";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(145, 216);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(87, 25);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(286, 210);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(150, 31);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(286, 135);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 31);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(180, 282);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(47, 25);
+            this.lblMsg.TabIndex = 4;
+            this.lblMsg.Text = "Msg";
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Location = new System.Drawing.Point(274, 345);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(112, 34);
+            this.btnSignin.TabIndex = 5;
+            this.btnSignin.Text = "Signin";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            this.btnSignin.Click += new System.EventHandler(this.btnSignin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnSignin;
+    }
+}
+
---
+++ EFAssessment/EFAssessment/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+namespace EFAssessment
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;database=Training;Integrated Security=True");
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            //parameterized query
+            string sql = "select count(*) from userdata where username=@name and password=@pwd";
+            SqlCommand command = new SqlCommand(sql, connection);
+            //pass value for the parameter
+            command.Parameters.AddWithValue("name", txtUserName.Text);
+            command.Parameters.AddWithValue("pwd", txtPassword.Text);
+            //open connection
+            connection.Open();
+            int count = (int)command.ExecuteScalar();
+            connection.Close();
+            if (count == 1)
+            {
+                lblMsg.Text = "valid User";
+                lblMsg.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                lblMsg.Text = "Invalid Username or password";
+                lblMsg.ForeColor = Color.Red;
+            }
+        }
+    }
+}
